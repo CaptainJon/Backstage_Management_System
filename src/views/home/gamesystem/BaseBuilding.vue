397c7f50397966cd09ded2540ba2a12c39707670
NeilChoi <neilchoi4you@163.com>
2020-01-11 16:12:18 +0800

    add formatter for avglevel

--- src/views/home/gamesystem/BaseBuilding.vue
+++ src/views/home/gamesystem/BaseBuilding.vue
@@ -6,9 +6,9 @@
       <el-breadcrumb-item>新手引导</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
-      <el-table :data="baseBuildingData" border style="width: 100%" size="mini" stripe highlight-current-row>
+      <el-table :data="baseBuildingData" border style="width: 100%" size="mini" stripe v-loading="loading">
         <el-table-column label="#" align="center" type="index"></el-table-column>
-        <el-table-column label="等级" align="center" prop="level"></el-table-column>
+        <el-table-column label="等级" align="center" prop="level" :formatter="avgFormat"></el-table-column>
         <el-table-column label="中枢系统" align="center" prop="avgCentral"></el-table-column>
         <el-table-column label="研发部" align="center" prop="avgResearch"></el-table-column>
         <el-table-column label="工程部" align="center" prop="avgEngine"></el-table-column>
@@ -24,14 +24,26 @@
 export default {
   data() {
     return {
-      baseBuildingData: []
+      baseBuildingData: [],
+      loading: true
     }
   },
   methods: {
+    // 数据获取
     getBaseBuildingData() {
       this.$http.get('/api/gadminc/business/basement.json').then(res => {
-        this.baseBuildingData = res.data.baseBuilding
+        this.baseBuildingData = res.data
+        // 关闭Loading
+        this.loading = false
       })
+    },
+    // 平均等级格式化
+    avgFormat(row, column, cellValue, index) {
+      if (cellValue === '-') {
+        return '平均等级'
+      } else {
+        return cellValue
+      }
     }
   },
   created() {
@@ -41,5 +53,4 @@ export default {
 </script>
 
 <style>
-
 </style>
