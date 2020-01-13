68a889f629a9e3c5386bced7e93c3a6b7dcdc912
NeilChoi <neilchoi4you@163.com>
2020-01-13 16:14:55 +0800

    add pagination

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, MessageBox, Message, Row, Col, Avatar, Menu, MenuItem, Submenu, Header, Aside, Main, Container, Breadcrumb, BreadcrumbItem, Card, Table, TableColumn, DatePicker, Progress, ButtonGroup, Tooltip, RadioGroup, RadioButton, Select, Option, Dialog, Loading } from 'element-ui'
+import { Button, Form, FormItem, Input, MessageBox, Message, Row, Col, Avatar, Menu, MenuItem, Submenu, Header, Aside, Main, Container, Breadcrumb, BreadcrumbItem, Card, Table, TableColumn, DatePicker, Progress, ButtonGroup, Tooltip, RadioGroup, RadioButton, Select, Option, Dialog, Loading, Pagination } from 'element-ui'
 
 Vue.use(Container)
 Vue.use(Header)
@@ -30,6 +30,7 @@ Vue.use(Select)
 Vue.use(Option)
 Vue.use(Dialog)
 Vue.use(Loading)
+Vue.use(Pagination)
 Vue.prototype.$confirm = MessageBox.confirm
 Vue.prototype.$alert = MessageBox.alert
 Vue.prototype.$message = Message
