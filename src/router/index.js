21a71be9e978504a95ad960bbb4f161488865690
NeilChoi <neilchoi4you@163.com>
2020-01-14 18:38:43 +0800

    add axios interceptors with token in request headers

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../views/login/Login.vue'
 
 Vue.use(VueRouter)
 
+// 路由配置
 const routes = [
   {
     path: '/',
@@ -12,21 +13,19 @@ const routes = [
   },
   {
     path: '/login',
-    name: 'login',
     component: Login
   },
   {
     path: '/home',
-    name: 'home',
     component: Home,
     children: [
       { path: '/newerguide', component: () => import(/* webpackChunkName: "gamesystem" */ '../views/home/gamesystem/NewerGuide.vue') },
       { path: '/heros', component: () => import(/* webpackChunkName: "gamesystem" */ '../views/home/gamesystem/Heros.vue') },
       { path: '/gamesystem', component: () => import(/* webpackChunkName: "gamesystem" */ '../views/home/gamesystem/GameSystem.vue') },
-      { path: '/corearmor', component: () => import(/* webpackChunkName: "gamesystem" */ '../views/home/gamesystem/CoreArmor.vue') },
-      { path: '/guildinfo', component: () => import(/* webpackChunkName: "gamesystem" */ '../views/home/gamesystem/GuildInfo.vue') },
-      { path: '/guildevent', component: () => import(/* webpackChunkName: "gamesystem" */ '../views/home/gamesystem/GuildEvent.vue') },
-      { path: '/basebuilding', component: () => import(/* webpackChunkName: "gamesystem" */ '../views/home/gamesystem/BaseBuilding.vue') },
+      { path: '/corearmor', component: () => import(/* webpackChunkName: "gamesystem2" */ '../views/home/gamesystem/CoreArmor.vue') },
+      { path: '/guildinfo', component: () => import(/* webpackChunkName: "gamesystem2" */ '../views/home/gamesystem/GuildInfo.vue') },
+      { path: '/guildevent', component: () => import(/* webpackChunkName: "gamesystem2" */ '../views/home/gamesystem/GuildEvent.vue') },
+      { path: '/basebuilding', component: () => import(/* webpackChunkName: "gamesystem2" */ '../views/home/gamesystem/BaseBuilding.vue') },
       { path: '/arena', component: () => import(/* webpackChunkName: "battlesystem" */ '../views/home/battlesystem/Arena.vue') },
       { path: '/mainstory', component: () => import(/* webpackChunkName: "battlesystem" */ '../views/home/battlesystem/MainStory.vue') },
       { path: '/apocalypse', component: () => import(/* webpackChunkName: "battlesystem" */ '../views/home/battlesystem/Apocalypse.vue') },
@@ -37,35 +36,38 @@ const routes = [
       { path: '/diamonds', component: () => import(/* webpackChunkName: "economy" */ '../views/home/economy/Diamonds.vue') },
       { path: '/newplayer', component: () => import(/* webpackChunkName: "operate" */ '../views/home/operate/NewPlayer.vue') },
       { path: '/onlinecalc', component: () => import(/* webpackChunkName: "operate" */ '../views/home/operate/OnlineCalc.vue') },
-      { path: '/activeplayer', component: () => import(/* webpackChunkName: "operate" */ '../views/home/operate/ActivePlayer.vue') },
-      { path: '/nextsaving', component: () => import(/* webpackChunkName: "operate" */ '../views/home/operate/NextSaving.vue') },
+      { path: '/activeplayer', component: () => import(/* webpackChunkName: "operate2" */ '../views/home/operate/ActivePlayer.vue') },
+      { path: '/nextsaving', component: () => import(/* webpackChunkName: "operate2" */ '../views/home/operate/NextSaving.vue') },
       { path: '/whitelist', component: () => import(/* webpackChunkName: "gm" */ '../views/home/gm/WhiteList.vue') },
       { path: '/sendbonus', component: () => import(/* webpackChunkName: "gm" */ '../views/home/gm/SendBonus.vue') },
       { path: '/behavelog', component: () => import(/* webpackChunkName: "dataquery" */ '../views/home/dataquery/BehaveLog.vue') },
-      { path: '/playermobile', component: () => import(/* webpackChunkName: "dataquery" */ '../views/home/dataquery/PlayerMobile.vue') }
+      { path: '/playermobile', component: () => import(/* webpackChunkName: "dataquery" */ '../views/home/dataquery/PlayerMobile.vue') },
+      { path: '/userlist', component: () => import(/* webpackChunkName: "account" */ '../views/home/account/UserList.vue') }
     ]
   }
 ]
 
+// 创建路由实例并挂载路由配置
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
-// 挂载路由拦截器-导航守卫
+// 路由拦截器
 router.beforeEach((to, from, next) => {
-  // 访问登录页则跳转登录页
+  // 1.获取token
+  // const tokenStr = document.cookie.split('=')[1]
+  const tokenStr = window.sessionStorage.getItem('gadmin-user')
+
+  // 2.访问登录页 ==> 跳转登录页
   if (to.path === '/login') {
     next()
   }
-  // 获取token
-  const tokenStr = document.cookie.split('=')[1]
-  // 判断是否拥有token
+
+  // 3.访问其他页面 ==> 验证token
   if (!tokenStr) {
-    next('/login') // 没有跳转登录页
+    next('/login')
   } else {
-    next() // 有则放行
+    next()
   }
 })
 
