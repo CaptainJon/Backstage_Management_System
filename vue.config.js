379c3cdf9d5fb2954185bb84285ba8d59696adaf
NeilChoi <neilchoi4you@163.com>
2020-01-14 18:39:38 +0800

    change proxy from online to local

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  // devServer: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://106.52.104.153:8281', // 外网IP切换
-  //       // target: "http://127.0.0.1:9090", 内网IP切换
-  //       changeOrigin: true,
-  //       ws: true,
-  //       pathRewrite: {
-  //         '/api': ''
-  //       }
-  //     }
-  //   }
-  // },
-  publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : './'
+  // 1.反向代理配置
+  devServer: {
+    proxy: {
+      '/app': {
+        // target: 'http://106.52.104.153:8281', // 外网IP切换
+        target: "http://127.0.0.1:9090", // 内网IP切换
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^/app': ''
+        }
+      }
+    }
+  },
+  // 2.资源公共路径设置
+  publicPath: process.env.NODE_ENV === 'production' ? '/dist' : './'
 }
